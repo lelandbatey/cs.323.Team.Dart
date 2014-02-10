f00ae000de478883a302db4fc70b994f8498ff22
Jason Wong <artezul@yahoo.com>
2014-02-10 12:02:37 -0800

    Renamed MissileManager to MissileLauncherManager.:

---
+++ Production/Src/SadCL/SadCL_Main/MissileLauncherManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SadCL
+{
+    class MissileLauncherManager
+    {
+        //Tells when the MissileLauncher to "Fire".
+        //Tells how much the MissileLauncher should "MoveBy".
+        //Tells where the MissileLauncher should "Move".
+        //Tells MissileLauncher to "Reload".
+        //Returns details about the MissileLauncher's "Status".
+        
+        //Converts from Cartesian to Spherical.
+        //Tracks number of missiles in launcher.
+        //Has launcher instance.
+        //Has launcher factory.
+        //Has launcher class definition - I think.
+
+    }
+}
