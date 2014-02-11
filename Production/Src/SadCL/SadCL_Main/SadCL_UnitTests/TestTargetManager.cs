092a92e2859651f2e6c10f9419f035751a31376f
lelandbatey <lelandbatey@gmail.com>
2014-02-10 18:31:21 -0800

    Expanded tests on TargetManager, pruned some code.
    Additionally, added ability to make mutableTarget out of normal Targets. This makes changing Targets possible, while still keeping them immutable.

---
+++ Production/Src/SadCL/SadCL_Main/SadCL_UnitTests/TestTargetManager.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SadCL_UnitTests
+{
+    /// <summary>
+    /// Summary description for TestTargetManager
+    /// </summary>
+    [TestClass]
+    public class TestTargetManager
+    {
+        public TestTargetManager() {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext {
+            get {
+                return testContextInstance;
+            }
+            set {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void takeAim_CheckAllMembersPresent() {
+            // Tests to see if all the proper named targets are found via TargetManager
+
+            string targPath = "validTargets.ini";
+            
+            Target.TargetManager tMan = Target.TargetManager.Instance;
+            tMan.load(GetTestData.getTestFilePath(targPath));
+
+            string[] targNames = new string[] { "target2", "ronni", "patrick", "lionel", "elnora", "mai" }; // All these are names of targets that are in fact in the file
+
+            foreach (var item in targNames) {
+                Assert.IsNotNull(tMan.find(item).Count);
+            }
+        }
+
+        [TestMethod]
+        public void takeAim_TestShootEnemies() {
+            // Checks that takeAim and find return the same objects
+            string targPath = "validTargets.ini";
+
+            Target.TargetManager tMan = Target.TargetManager.Instance;
+            tMan.load(GetTestData.getTestFilePath(targPath));
+            string[] enemyNames = new string[] { "target2", "ronni", "patrick", "elnora", "mai" }; // A list of all the targets in the file who have "Friend=False" as their attributes
+
+            foreach (var item in enemyNames) {
+                Assert.AreEqual(tMan.find(item)[0], tMan.takeAim(item));
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void takeAim_TestShootFriend() {
+            // Checks that attempting to shoot a friend will raise an error
+            string targPath = "validTargets.ini";
+
+            Target.TargetManager tMan = Target.TargetManager.Instance;
+            tMan.load(GetTestData.getTestFilePath(targPath));
+            string[] friendNames = new string[] { "Lionel" };
+
+            foreach (var item in friendNames) {
+                tMan.takeAim(item);
+            }
+        }
+
+        [TestMethod]
+        public void Target_TestConverion() {
+            string targPath = "validTargets.ini";
+
+            Target.TargetManager tMan = Target.TargetManager.Instance;
+            tMan.load(GetTestData.getTestFilePath(targPath));
+
+            Target.Target j1 = tMan.find("mai")[0];
+            Target.mutableTarget m1 = new Target.mutableTarget(j1);
+            Target.Target j2 = new Target.Target(m1);
+            Target.mutableTarget m2 = new Target.mutableTarget(j2);
+
+            Console.WriteLine(j1);
+            Console.WriteLine(j2);
+            Console.WriteLine();
+            Console.WriteLine(m1);
+            Console.WriteLine(m2);
+
+            Assert.AreEqual(j1.ToString(), j2.ToString());
+            Assert.AreEqual(m1.ToString(), m2.ToString());
+        }
+
+    }
+}
--- Production/Src/SadCL/SadCL_Main/TargetManager.cs
+++ Production/Src/SadCL/SadCL_Main/TargetManager.cs
@@ -40,17 +40,33 @@ namespace Target
         }
 
         public List<Target> find(string name) { // Searches for all targets that have a given name
+            name = name.ToLower();
             List<Target> toRet = new List<Target>();
             toRet = masterList.FindAll(s => s.Name == name);
             return toRet;
         }
+
+        // While "find" naively returns all targets with a given name, "takeAim" does some checking to make sure the target we're looking for is one we're actually allowed to shoot at.
+        public Target takeAim(string name) {
+            List<Target> toRet = new List<Target>();
+            if (find(name).Count == 0) {
+                throw new ArgumentException("No target by that name");
+            }
+            Target tempTarg = find(name)[0];
+
+            if (tempTarg.Friend == true) {
+                throw new ArgumentOutOfRangeException("Target is friendly");
+            }
+            return tempTarg;
+        }
+
         public void printEnemies() { }
         public void printFriends() { }
 
         // Private method declarations
         private void print(List<Target> targList ) {
             foreach (var item in targList) {
-                
+                Console.WriteLine(item);
             }
         }
         private List<Target> listEnemies() {
@@ -62,6 +78,13 @@ namespace Target
             List<Target> toRet = new List<Target>();
             toRet = masterList.FindAll(s => s.Friend == true);
             return toRet;
+        }
+
+        private void setToDead(string name) {
+            List<Target> oldList = masterList;
+            List<Target> newList = masterList;
+
+            int replaceIndex = masterList.FindIndex(s => s.Name == name);
         }
         
     }    
