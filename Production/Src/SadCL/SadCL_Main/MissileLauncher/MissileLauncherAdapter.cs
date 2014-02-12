9115b2d63c03dc7528b0685bbe5613fc74648eb5
Jason Wong <artezul@yahoo.com>
2014-02-12 13:12:45 -0800

    Moved MissileLauncherAdapter to its own .cs file.

---
+++ Production/Src/SadCL/SadCL_Main/MissileLauncher/MissileLauncherAdapter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SadCL.MissileLauncher
+{
+    public class MissileLauncherAdapter
+    {
+        public string launcherName { get; set; }
+        public int launcherAmmo { get; set; }
+        public void fire()
+        {
+            command_Fire();
+        }
+
+        public void reload()
+        {
+            System.Console.WriteLine("Insert Sound of Shotgun Reloading.");
+        }
+
+        public void status()
+        {
+            System.Console.WriteLine("My status is that I have no status.");
+        }
+    }
+}
--- Production/Src/SadCL/SadCL_Main/MissileLauncher/MissileLauncherManager.cs
+++ Production/Src/SadCL/SadCL_Main/MissileLauncher/MissileLauncherManager.cs
@@ -22,23 +22,5 @@ namespace SadCL
 
     }    
 
-    public class MissileLauncherAdapter : MissileLauncherHardware
-    {
-        public string launcherName { get; set; }
-        public int launcherAmmo { get; set; }
-        public void fire()
-        {
-            command_Fire();
-        }
-
-        public void reload()
-        {
-            System.Console.WriteLine("Insert Sound of Shotgun Reloading.");
-        }
-
-        public void status()
-        {
-            System.Console.WriteLine("My status is that I have no status.");
-        }
-    }
+   
 }
