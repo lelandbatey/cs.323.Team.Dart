453b9e7e677733eee246ed901c90f4568788e5e6
Jason Wong <artezul@yahoo.com>
2014-02-12 13:00:13 -0800

    Moved Missile Launcher Factory code to its own .cs file.

---
+++ Production/Src/SadCL/SadCL_Main/MissileLauncher/MissileLauncherFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SadCL.MissileLauncher
+{
+    public class MissileLauncherFactory
+    {
+        public enum LauncherTypes
+        {
+            Mock = 0,
+            DreamCheeky = 1
+        }
+
+        public MissileLauncherAdapter create_Launcher(LauncherTypes RocketProduct)
+        {
+            if (RocketProduct == LauncherTypes.DreamCheeky)
+                return new DreamCheekyLauncher("HULK", 4);
+            //else if (RocketProduct == LauncherTypes.Mock)
+            //    producedObject = new Mock;
+            else
+                System.Console.WriteLine("Unknown enumeration passed to factory.");
+
+            return new DreamCheekyLauncher("Sauron", 1);
+        }
+    }
+}
