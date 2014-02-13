80e6170e14f721ac0c04d8ce0b34b9d32d8cc4b8
Jason Wong <artezul@yahoo.com>
2014-02-13 00:26:56 -0800

    Added DreamCheeky / Mock subclasses.

--- Production/Src/SadCL/SadCL_Main/MissileLauncher/MissileLauncherAdapter.cs
+++ Production/Src/SadCL/SadCL_Main/MissileLauncher/MissileLauncherAdapter.cs
@@ -8,22 +8,8 @@ namespace SadCL.MissileLauncher
 {
     //The Adapter inherits from both the legacy hardware, and the current format for
     //all missile launchers (Interface).
-    public class MissileLauncherAdapter : MissileLauncherHardware, IMissileLauncher
+    abstract class MissileLauncherAdapter : MissileLauncherHardware, IMissileLauncher
     {
-        public string launcherName { get; private set; }
-        public int launcherAmmo { get; private set; }
-
-        public MissileLauncherAdapter(string passedName, int passedAmmo)
-        {
-            launcherName = passedName;
-            launcherAmmo = passedAmmo;
-        }
-
-        public void fire()
-        {
-            command_Fire();
-        }
-
         public void moveBy(double phi, double theta)
         {
             if (phi < 0.0)
@@ -44,20 +30,79 @@ namespace SadCL.MissileLauncher
                 command_Up((int)theta);
             }
         }
+    }
+
+    public class DreamCheekyLauncher : MissileLauncherAdapter
+    {
+        public string launcherName { get; private set; }
+        public int launcherAmmo { get; private set; }
+
+        //This is the max ammo across all DreamCheekys.
+        private static int MAX_AMMO = 4;
+        private static int OUT_OF_AMMO = 0;
+
+        public DreamCheekyLauncher(string passedName, int passedAmmo)
+        {
+            launcherName = passedName;
+            launcherAmmo = passedAmmo;
+        }
+
+        public void fire()
+        {
+            if (launcherAmmo == OUT_OF_AMMO)
+            {
+                System.Console.WriteLine("You must acquire more Vespene Gas! (Ammo)");
+            }
+            else
+            {
+                command_Fire();
+                --launcherAmmo;
+            }     
+        }
 
         public void reload()
         {
-            System.Console.WriteLine("Insert Sound of Shotgun Reloading.");
+            launcherAmmo = MAX_AMMO;
         }
 
         public void status()
         {
-            System.Console.WriteLine("My status is that I have no status.");
+            System.Console.WriteLine("Name: {0}", launcherName);
+            System.Console.WriteLine("Ammo: {0}", launcherAmmo);
         }
 
         public void reset()
         {
+            System.Console.WriteLine("Please wait while we return to origin.");
             command_reset();
+        }
+    }
+
+    public class MockLauncher : MissileLauncherAdapter
+    {
+        public string launcherName { get; private set; }
+        public int launcherAmmo { get; private set; }
+        public void fire()
+        {
+            System.Console.WriteLine("PEW, PEW, PEW, PEW!!!");
+        }
+        void moveBy(double phi, double theta)
+        {
+            System.Console.WriteLine("Theta: {0}", theta);
+            System.Console.WriteLine("Phi: {0}", phi);
+        }
+        void reload()
+        {
+            System.Console.WriteLine("Adding more arrows to your quiver!");
+        }
+        void status()
+        {
+            System.Console.WriteLine("I was once known as {0}", launcherName);
+            System.Console.WriteLine("There are only {0} arrows left in my quiver!", launcherAmmo);
+        }
+        void reset()
+        {
+            System.Console.WriteLine("To reset.  Or not to reset.  That.. is the question.");
         }
     }
 }
