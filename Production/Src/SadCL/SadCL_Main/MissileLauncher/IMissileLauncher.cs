058a0dea3df54d83855ebfb9397a0ff743b3d30c
Jason Wong <artezul@yahoo.com>
2014-02-13 11:42:36 -0800

    Changed the position of the phi and theta var.
    Typically spherical coordinates are represented
    
    as (rho, theta, phi), and I wanted to mirror that.

--- Production/Src/SadCL/SadCL_Main/MissileLauncher/IMissileLauncher.cs
+++ Production/Src/SadCL/SadCL_Main/MissileLauncher/IMissileLauncher.cs
@@ -4,7 +4,7 @@ namespace SadCL.MissileLauncher
     public interface IMissileLauncher
     {
         void fire();
-        void moveBy(double phi, double theta);
+        void moveBy(double theta, double phi);
         void reload();
         void status();
         void reset();   //Though it isn't a requirement, it's convenient to have.
