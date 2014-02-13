9f5b1e4b60aeac1fdfbd8063cb35dd325addd523
Jason Wong <artezul@yahoo.com>
2014-02-13 00:52:27 -0800

    Implemented rest of Missile Launcher Factory.

--- Production/Src/SadCL/SadCL_Main/MissileLauncher/MissileLauncherFactory.cs
+++ Production/Src/SadCL/SadCL_Main/MissileLauncher/MissileLauncherFactory.cs
@@ -6,23 +6,34 @@ using System.Threading.Tasks;
 
 namespace SadCL.MissileLauncher
 {
-    public enum LauncherTypes
+    enum LauncherTypes
     {
         Mock = 0,
         DreamCheeky = 1
+    }
+    enum AmmoCount
+    {
+        EmptyAmmo = 0,
+        MaxAmmo = 4
     }
     public class MissileLauncherFactory
     {
         public static IMissileLauncher create_Launcher(LauncherTypes Product)
         {
-            //if (Product == LauncherTypes.DreamCheeky)
-            //    return new DreamCheekyLauncher("HULK", 4);
-            //else if (Product == LauncherTypes.Mock)
-            //    producedObject = new Mock;
-            //else
-            //    System.Console.WriteLine("Unknown enumeration passed to factory.");
+            IMissileLauncher generatedLauncher = null;
 
-            return new MissileLauncherAdapter ("Sauron", 1);
+            if (Product == LauncherTypes.Mock)
+            {
+                generatedLauncher = new MockLauncher();
+            }
+            else if (Product == LauncherTypes.DreamCheeky)
+            {
+                generatedLauncher = new DreamCheekyLauncher("Photon Cannon", (int)AmmoCount.MaxAmmo);
+            }
+            else
+                throw new ArgumentException("The product you want me to create doesn't exist in my database.");
+
+            return generatedLauncher;
         }
     }
 }
