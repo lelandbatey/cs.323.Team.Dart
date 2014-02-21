92da5a29106cfb1e7f85127e8ad9a4da75cb6b82
Jason Wong <artezul@yahoo.com>
2014-02-21 15:51:49 -0800

    Relocated Mock Object to own .cs file.
    Easier to locate and change if necessary.

--- Production/Src/SadCL/SadCL_Main/MissileLauncher/MissileLauncherAdapter.cs
+++ Production/Src/SadCL/SadCL_Main/MissileLauncher/MissileLauncherAdapter.cs
@@ -38,40 +38,4 @@ namespace SadCL.MissileLauncher
             }
         }
     }
-
-    
-
-    public class MockLauncher : IMissileLauncher
-    {
-        public string launcherName { get; private set; }
-        public int launcherAmmo { get; private set; }
-
-        public MockLauncher()
-        {
-            launcherName = "King Henry";
-            launcherAmmo = 5;
-        }
-        public void fire()
-        {
-            System.Console.WriteLine("Cry 'God for Harry! England and Saint George!'");
-        }
-        new public void moveBy(double phi, double theta)
-        {
-            System.Console.WriteLine("Theta: {0}", theta);
-            System.Console.WriteLine("Phi: {0}", phi);
-        }
-        public void reload()
-        {
-            System.Console.WriteLine("Once more unto the breach, dear friends, once more.");
-        }
-        public void status()
-        {
-            System.Console.WriteLine("I was once known as {0}", launcherName);
-            System.Console.WriteLine("There are only {0} arrows left in my quiver!", launcherAmmo);
-        }
-        public void reset()
-        {
-            System.Console.WriteLine("To reset.  Or not to reset.  That.. is the question.");
-        }
-    }
 }
