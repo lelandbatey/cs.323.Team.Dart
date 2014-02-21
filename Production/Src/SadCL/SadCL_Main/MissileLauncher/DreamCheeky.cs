ae3e6c26b9691dfe6edb25cffd7b90b2f28b9a2c
Jason Wong <artezul@yahoo.com>
2014-02-21 15:47:02 -0800

    Relocated DreamCheeky object to own .cs file.
    Often forgot the DreamCheeky / Mock were stored in
    
    Adapter file.

---
+++ Production/Src/SadCL/SadCL_Main/MissileLauncher/DreamCheeky.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SadCL.MissileLauncher
+{
+    public class DreamCheekyLauncher : MissileLauncherAdapter, IMissileLauncher
+    {
+        public string launcherName { get; private set; }
+        public int launcherAmmo { get; private set; }
+
+        public DreamCheekyLauncher(string passedName, int passedAmmo) {
+            launcherName = passedName;
+            launcherAmmo = passedAmmo;
+        }
+
+        public void fire() {
+            if (launcherAmmo == (int)AmmoCount.EmptyAmmo) {
+                System.Console.WriteLine("We're out of ammunition!");
+            } else {
+                System.Console.WriteLine("PEW!");
+                command_Fire();
+                --launcherAmmo;
+            }
+        }
+
+        public void reload() {
+            System.Console.WriteLine("We're reloaded!");
+            launcherAmmo = (int)AmmoCount.MaxAmmo;
+        }
+
+        public void status() {
+            System.Console.WriteLine("Name: {0}", launcherName);
+            System.Console.WriteLine("Ammo: {0}", launcherAmmo);
+        }
+
+        public void reset() {
+            System.Console.WriteLine("Please wait while we return to origin.");
+            command_reset();
+            System.Console.WriteLine("Reset Complete!");
+        }
+    }
+}
