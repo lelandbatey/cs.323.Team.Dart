82d57803a7e49f511e5450a8c28533ca9f9e8a2a
Jason Wong <artezul@yahoo.com>
2014-02-22 22:59:15 -0800

    Modified IMissileLauncher to include move(To).
    Also swapped the Theta and Phi variables in moveBy
    
    to Phi and Theta to keep it consistent with the
    
    rest of the program.

--- Production/Src/SadCL/SadCL_Main/MissileLauncher/IMissileLauncher.cs
+++ Production/Src/SadCL/SadCL_Main/MissileLauncher/IMissileLauncher.cs
@@ -4,7 +4,9 @@ namespace SadCL.MissileLauncher
     public interface IMissileLauncher
     {
         void fire();
-        void moveBy(double theta, double phi);
+
+        void move(double phi, double theta);
+        void moveBy(double phi, double theta);
         void reload();
         void status();
         void reset();   //Though it isn't a requirement, it's convenient to have.
