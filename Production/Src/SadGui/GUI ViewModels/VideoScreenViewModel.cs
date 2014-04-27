f8dac1a4ef1fbafda58cc532d4431d26449b2ce2
Jason Wong <artezul@yahoo.com>
2014-04-26 17:55:34 -0700

    Made changes to VideoControl; less verbose code.

---
+++ Production/Src/SadGui/GUI ViewModels/VideoScreenViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SadCLGUI.GUI_ViewModels
+{
+    class VideoScreenViewModel
+    {   
+        Capture m_camera;
+        bool m_isProcessing;
+
+        public VideoScreenViewModel()
+        {
+            m_isProcessing  = false;
+            m_camera        = new Capture();
+        }
+
+    }
+}
--- Production/Src/SadGui/GUI Views/VideoControl.xaml.cs
+++ Production/Src/SadGui/GUI Views/VideoControl.xaml.cs
@@ -31,12 +31,16 @@ namespace SadCLGUI.GUI_Views
 			//}
 		}
 
-		private void StopButton_Click(object sender, RoutedEventArgs e) {
-			VideoControlViewModel.Singleton.StopVideo();
-		}
+        //private void StopButton_Click(object sender, RoutedEventArgs e) {
+        //    VideoControlViewModel.Singleton.StopVideo();
+        //}
 
-		private void StartButton_Click(object sender, RoutedEventArgs e) {
-			VideoControlViewModel.Singleton.Start(image);
-		}
+        //private void StartButton_Click(object sender, RoutedEventArgs e) {
+        //    VideoControlViewModel.Singleton.Start(image);
+        //}
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            VideoControlViewModel.Singleton.CameraButton(CamImageBox);
+        }
 	}
 }
